f55251f3c691af0854f4f0111fbf844b86cf3058
vinhweb <vjhnguyen@gmail.com>
2024-04-13 02:18:14 +0700

    og-image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,11 @@ import {Metadata} from "next";
 export const metadata: Metadata = {
   title: 'Tool Download nhiều hình ảnh một lúc',
   description: 'Đây là một tool siêu đơn giản, giúp bạn download nhiều hình ảnh một lúc bằng đoạn văn bản chứa URLs hình ảnh mà bạn muốn tải về',
+  openGraph: {
+    title: 'Tool Download nhiều hình ảnh một lúc',
+    description: 'Đây là một tool siêu đơn giản, giúp bạn download nhiều hình ảnh một lúc bằng đoạn văn bản chứa URLs hình ảnh mà bạn muốn tải về',
+    images: process.env.SITE_URL+'/og-image.jpg'
+  }
 }
 
 export default function Home() {
