232fa869182b88ff785cc6581c89f5d13626a358
vinhweb <vjhnguyen@gmail.com>
2024-04-13 02:16:36 +0700

    og-image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,11 @@ import {Metadata} from "next";
 export const metadata: Metadata = {
   title: 'Download multiple images at once',
   description: 'This is a super simple tool that helps you download multiple images at once using text containing the URLs of the images you want to download.',
+  openGraph: {
+    title: 'Download multiple images at once',
+    description: 'This is a super simple tool that helps you download multiple images at once using text containing the URLs of the images you want to download.',
+    images: process.env.SITE_URL+'/og-image.jpg'
+  }
 }
 
 export default function Home() {
